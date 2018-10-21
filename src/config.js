da88205e3711a01d38ba892ed43aad2e75181cf5
chukwujohnpaul <jchukwu@teamapt.com>
2018-10-21 04:16:39 +0100

    corrected db changes

--- src/config.js
+++ src/config.js
@@ -21,3 +21,5 @@ export const url = 'http://localhost:3001';
 //
 // export const s3Region = 'us-west-2'
 // export const s3Bucket = 'fileapp'
+
+export const dbUrl = process.env.NODE_ENV.SCALINGO_MONGO_URL;
--- src/database.js
+++ src/database.js
@@ -1,12 +1,13 @@
 import {MongoClient} from 'mongodb'
+import {dbUrl} from './config'
 
 
-const url = 'mongodb://localhost:27017/fileapp';
+// const url = 'mongodb://localhost:27017/fileapp';
 
 
 export const connect = (callback) => {
 
-    MongoClient.connect(url, (err, db) =>
+    MongoClient.connect(dbUrl, (err, db) =>
     {
         return callback(err, db);
 
