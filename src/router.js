2bb1eed6d0fa92e84fc892c7e3bee22fae92e5ec
johnpaulchukwu43 <johnpaulchukwu43@gmail.com>
2018-10-21 04:53:18 +0100

    Update router.js
    Added support for cors

--- src/router.js
+++ src/router.js
@@ -26,6 +26,12 @@ class AppRouter {
         const db = app.get('db');
         const uploadDir = app.get('storageDir');
         const upload = app.upload;
+        
+        app.use(function(req, res, next) {
+          res.header("Access-Control-Allow-Origin", "*");
+          res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+          next();
+        });
 
         // root routing.
         app.get('/', (req, res, next) => {
